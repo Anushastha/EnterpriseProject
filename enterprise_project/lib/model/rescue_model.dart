4e75df101e6aa21fc8003ed2f816a7c0a39dfdf9
Anushastha <shresthanu123@gmail.com>
2023-07-07 20:45:24 +0545

    team list

---
+++ enterprise_project/lib/model/rescue_model.dart
@@ -0,0 +1,23 @@
+class Rescue {
+  String firstName;
+  String lastName;
+  String middleName;
+  String password;
+  String address;
+  String contactNo;
+  String gender;
+  String dob;
+  String role;
+
+  Rescue({
+    required this.firstName,
+    required this.lastName,
+    required this.middleName,
+    required this.password,
+    required this.address,
+    required this.contactNo,
+    required this.gender,
+    required this.dob,
+    required this.role,
+  });
+}
